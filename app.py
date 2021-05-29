857b3a28c289d46ca84d6bfceaf94b0b505ae491
nikhilnano <nikhil.saini39@gmail.com>
2021-05-29 20:26:20 +0530

    added procfile and req.txt

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ pred = Predictor()
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-    print(data)
     output = pred.get_final_labels_in_video(data['url'])
-    print(output)
     return jsonify(output)
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=3000)
+    app.run()
